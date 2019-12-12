5b591ef8f3e90aeb1e2907697e0251072b646d29
Yury Kastsen <yurykstn@gmail.com>
2019-12-13 00:35:22 +0300

    changes

--- src/components/PwaReliable.js
+++ src/components/PwaReliable.js
@@ -1,60 +1,27 @@
 import React from "react"
 
 import './pwaReliable.scss'
-
 import pwaImg from './../images/magento-2-pwa-theme.png'
-import pwaImg2 from './../images/what-is-a-pwa.png';
-import pwaImg3 from './../images/5_graphic_PWA_rus.webp';
 
 import SectionLayout from "./sectionLayout";
 
 const PwaReliable =() => (
-    <>
-        <SectionLayout name="pwaReliable">
-            <div className="col-lg-12 text-center">
-                <h2>
-                    Why build a Progressive Web App?
-                </h2>
-            </div>
-            <div className="col-lg-6">
-                <img src={pwaImg} alt=""/>
-            </div>
-            <div className="col-lg-6 row middle-xs">
-                <div className="box">
-                    <h3>PWA is one of the most talked about technology shifts in the web</h3>
-                    <p>
-                        If you are building for the web, PWA has incredible benefits, making it easy to delight your users, grow engagement and increase conversions.
-                    </p>
-                </div>
-            </div>
-            <div className="col-lg-6 row middle-xs">
-                <div className="box">
-                    <h3>Work reliably, no matter the network conditions</h3>
-                    <p>
-                        More than 40% of the users bounce from the websites that take more than 3 seconds to load.
-                    </p>
-                    <p>
-                        PWA is a solution for this “Web Obesity Epidemic” faced by the users.
-                    </p>
-                </div>
-            </div>
-            <div className="col-lg-6">
-                <img src={pwaImg2} alt=""/>
-            </div>
-
-            <div className="col-lg-6 text-center">
-                <img src={pwaImg3} alt=""/>
-            </div>
-            <div className="col-lg-6 row middle-xs">
-                <div className="box">
-                    <h3>Why is it important?</h3>
-                    <p>
-                        PWA reduces the steps between discovery of an app and getting it on the home screen and thereby eliminates friction of getting an app installed.
-                    </p>
-                </div>
-            </div>
-        </SectionLayout>
-    </>
+  <SectionLayout name="pwaReliable">
+      <div className="row">
+          <div className="col-lg-6">
+                  <h2>
+                      Why do we need a <br/>
+                    Progressive App?
+                  </h2>
+                  <p>
+                      Even though Progressive Web Apps build on <br/>
+                      new technologies, they are already widely <br/>
+                      in use in the industry. Start with building <br/>
+                    a great web experience.
+                  </p>
+          </div>
+      </div>
+  </SectionLayout>
 );
 
 export default PwaReliable
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,14 @@ import Features from "../components/Features";
 import TopSection from "../components/TopSection";
 // import SectionLayout from "../components/sectionLayout";
 import Technologies from "../components/Technologies"
+import PwaReliable from "../components/PwaReliable"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Progressive Web Apps for business" />
     <TopSection/>
     <Features/>
+    <PwaReliable/>
     <Technologies/>
     {/*<SectionLayout name="test">*/}
     {/*    <Link to="/page-2/">Go to page 2</Link>*/}
