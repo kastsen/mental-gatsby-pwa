a0647ab2b5192e2a33a3498ed0e5e643f07efff1
Yury Kastsen <yurykstn@gmail.com>
2020-01-05 10:53:38 +0300

    Update sectionLayout.js

--- src/components/sectionLayout.js
+++ src/components/sectionLayout.js
@@ -1,22 +1,14 @@
 import React from "react";
 
 const SectionLayout = (props) => {
-
     return (
-        <>
-            <section className={props.name}>
-                <div className="container">
-                    <div className="content">
-
-                            {props.children}
-
-                    </div>
-                </div>
-            </section>
-        </>
+              <section className={props.name}>
+                  <div className="container">
+                      <div className="content">
+                              {props.children}
+                      </div>
+                  </div>
+              </section>
     )
-};
-
-
-
+}
 export default SectionLayout
