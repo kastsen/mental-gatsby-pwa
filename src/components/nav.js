1a018b764ca2b4aa027fee2f7b7548ce3a435e32
Yury Kastenedev <yurykstn@gmail.com>
2021-11-10 14:59:54 +0300

    update contact us link

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,13 @@
 import { Link } from "gatsby"
 import React from "react"
-import { hideScroll } from './hideScroll'
 
 const Nav =() => (
     <nav>
         <ul>
             <li><Link to="#services">Services</Link></li>
             <li><a href="/pricing/">Pricing</a></li>
-          <li><a href="/blog/">Blog</a></li>
-            <li className={'contactUs'}>
-                <Link
-                  to="/requestForm/"
-                  state={{
-                      modal: true,
-                      noScroll: true,
-                  }} onClick={hideScroll}>Contact</Link>
-            </li>
+            <li><a href="/blog/">Blog</a></li>
+            <li className={'contactUs'}><Link to="/requestForm/">Contact</Link></li>
         </ul>
     </nav>
 )
