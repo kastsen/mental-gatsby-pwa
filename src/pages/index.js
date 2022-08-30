2180dd9140022a119a3de46f4237bd2944548615
Yury Kastenedev <yurykstn@gmail.com>
2022-08-30 14:01:26 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import SEO from "../components/seo"
 import Features from "../components/Features";
 import HeadSection from "../components/HeadSection";
 import Technologies from "../components/Technologies"
-import PwaReliable from "../components/PwaReliable"
-import { data } from "../data/data"
 
 const IndexPage = () => (
   <Layout>
