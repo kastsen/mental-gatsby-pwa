b6214ff4a6b3e4ecb760ba5ea9a6fdb4c597d213
Yury Kastenedev <yurykstn@gmail.com>
2022-08-31 20:08:07 +0200

    add CV

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,27 @@
+import gitIcon from "../images/github.png"
+import linkedinIcon from "../images/linkedin.png"
+import upworkIcon from "../images/upwork.svg"
+import cvIcon from "../images/cv.png"
+import React from "react"
+import '../scss/socialIcons.scss'
+
+const SocialIcons =() => (
+  <>
+    <div className="socialIcons">
+      <a href="https://github.com/kastsen" target="_blank">
+        <img src={gitIcon} alt='git' className='social-icon invert' width="32" height='auto'/>
+      </a>
+      <a href="https://www.linkedin.com/in/kastsen/" target="_blank">
+        <img src={linkedinIcon} alt='linkedin' className='social-icon invert linkedin' width="30" height='auto'/>
+      </a>
+      <a href="https://www.upwork.com/freelancers/~01ffc76279a8057a44" target="_blank">
+        <img src={upworkIcon} alt='upwork' className='social-icon invert upwork' width="30" height='auto'/>
+      </a>
+      <a href="/Yury-Kastsen-CV-31-08-2022.pdf" target="_blank">
+        <img src={cvIcon} alt='download cv' className='social-icon invert cv' width="32" height='auto'/>
+      </a>
+    </div>
+  </>
+);
+
+export default SocialIcons
