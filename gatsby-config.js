e27cd934c5750c91b00d57fe88fd2cb0535d0263
Yury Kastenedev <yurykstn@gmail.com>
2022-09-09 11:13:29 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,13 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-239906189-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
