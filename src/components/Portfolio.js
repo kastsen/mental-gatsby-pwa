197e7d2f5d17ea70884e9ddf7d961f026c3175b2
Yury Kastenedev <yurykstn@gmail.com>
2022-10-09 18:04:31 +0200

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ const Portfolio = ({ title}) =>
             <div className='text-center'>
                 <h2 id='portfolio'>{title}</h2>
                 <p className='subtitle'>
-                    From interaction design to scaleable design systems, single-page apps to something more experimental with WebGL. <strong>My highest priority:</strong> working for clients around the globe.
+                    From interaction design to scaleable design systems, single-page apps to something more experimental with WebGL. For clients around the globe.
                 </p>
             </div>
 
