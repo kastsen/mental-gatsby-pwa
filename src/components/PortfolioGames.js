9f3636b9d31e5843c8c4fd3f12dec5e349cb7780
Yury Kastsen <yurykstn@gmail.com>
2023-06-22 17:55:05 +0200

    Portfolio optimization

--- src/components/PortfolioGames.js
+++ src/components/PortfolioGames.js
@@ -1,27 +1,33 @@
-import React from "react"
-import icon from '../images/games.png'
+import React from "react";
+import icon from '../images/games.png';
+import screenshotSheep from '../images/screenshot-bears-and-sheep-playable-vizor-games.png';
+import screenshotCactus from '../images/screenshot-cactus-playable-vizor-games.png';
 
 const PortfolioGames = () =>
-  <>
-    <div className="text-center">
-      <img src={icon} alt='Playable Ads' loading='lazy' className='projectIcon' width="46" height='46'/>
-      <h3>
+	<>
+		<div className="text-center">
+			<img src={icon} alt='Playable Ads' loading='lazy' className='projectIcon' width="46" height='46'/>
+			<h3>
         Games & Playable Ads
-      </h3>
-    </div>
+			</h3>
+		</div>
 
-    <p className='reference text-center copyright'>
+		<p className='reference text-center copyright'>
       Art & concept by <a href="https://vizor-games.com/" target='_blank' rel="noreferrer">Vizor Games</a>
-    </p>
+		</p>
 
-    <div className="row projects playableAds active">
-      <div className="playable">
-        <iframe src="/p1664.html" loading='lazy' frameBorder="0" width="600" height="420" title="angry sheep"></iframe>
-      </div>
-      <div className="playable">
-        <iframe src="/p1825.html" loading='lazy' frameBorder="0" width="600" height="420" title="brainteaser"></iframe>
-      </div>
-    </div>
-  </>
+		<div className="row projects playableAds active">
+			<div className="playable">
+				<a href={'/p1664.html'}>
+					<img src={screenshotSheep} loading='lazy' width="600" title="angry sheep" alt={'angry sheep playable vizor games'}></img>
+				</a>
+			</div>
+			<div className="playable">
+				<a href={'/p1825.html'}>
+					<img src={screenshotCactus} loading='lazy' width="600" title="brainteaser" alt={'cactus playable vizor games'}></img>
+				</a>
+			</div>
+		</div>
+	</>;
 
 export default PortfolioGames;
