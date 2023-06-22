7c539a58e9908c0d3508e07f89c959f96afafaab
Yury Kastsen <yurykstn@gmail.com>
2023-06-22 18:59:10 +0200

    Fix request form icon close

--- src/pages/requestForm.js
+++ src/pages/requestForm.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { ModalRoutingContext } from 'gatsby-plugin-modal-routing'
-import '../scss/modalWindow.scss'
-import GetStartedForm from "../components/Form"
-import '../scss/ui.scss'
+import React from 'react';
+import { Link } from 'gatsby';
+import { ModalRoutingContext } from 'gatsby-plugin-modal-routing';
+import '../scss/modalWindow.scss';
+import GetStartedForm from "../components/Form";
+import '../scss/ui.scss';
 
 const visibleScroll = () => {
-  const page = document.querySelector('html')
-  page.style.overflow = 'visible';
-}
+	const page = document.querySelector('html');
+	page.style.overflow = 'visible';
+};
 
 const ModalExamplePage = () => (
-  <div className="requestForm">
-    <ModalRoutingContext.Consumer>
-      {({ modal, closeTo }) => (
-        <div className='form'>
-          {modal ? (
-            <Link to={closeTo} className='close' onClick={visibleScroll}>
-            </Link>
-          ) : (<></>)}
-          <h2>Contact Us</h2>
-          <GetStartedForm/>
-          <a href="/"><span className='closeIcon'>x</span></a>
-        </div>
-      )}
-    </ModalRoutingContext.Consumer>
-  </div>
-)
+	<div className="requestForm">
+		<ModalRoutingContext.Consumer>
+			{({ modal, closeTo }) => (
+				<div className='form'>
+					{modal ? (
+						<Link to={closeTo} className='close' onClick={visibleScroll}>
+						</Link>
+					) : (<></>)}
+					<h2>Contact Us</h2>
+					<GetStartedForm/>
+					<a href="/"><span className='closeIcon'></span></a>
+				</div>
+			)}
+		</ModalRoutingContext.Consumer>
+	</div>
+);
 
-export default ModalExamplePage
+export default ModalExamplePage;
